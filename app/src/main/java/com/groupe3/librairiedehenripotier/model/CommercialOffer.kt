5a459139c82526be26bdc3a5151e6958458bbf4e
Raphael Hascoet <hascoetraphel-29@yahoo.fr>
2020-11-16 17:36:10 +0100

    Added calculator tests

--- app/src/main/java/com/groupe3/librairiedehenripotier/model/CommercialOffer.kt
+++ app/src/main/java/com/groupe3/librairiedehenripotier/model/CommercialOffer.kt
@@ -1,3 +1,5 @@
 package com.groupe3.librairiedehenripotier.model
 
-data class CommercialOffer(val type: OfferType, val value: Float, val sliceValue: Float?)
+data class CommercialOffer(val type: OfferType, val value: Float, val sliceValue: Float?) {
+    constructor(type: OfferType, value: Float) : this(type, value, null)
+}
--- app/src/main/java/com/groupe3/librairiedehenripotier/utils/OfferCalculator.kt
+++ app/src/main/java/com/groupe3/librairiedehenripotier/utils/OfferCalculator.kt
@@ -7,22 +7,30 @@ import kotlin.math.floor
 
 object OfferCalculator {
 
-    fun getBestOfferWithPrice(books: List<Book>, offers: List<CommercialOffer>): Pair<CommercialOffer, Float> {
+    fun getBestOfferWithPrice(books: List<Book>, offers: List<CommercialOffer>): Pair<CommercialOffer?, Float> {
         val priceWithOffers = offers.map { offer -> getPriceWithOffer(books, offer) }
         val bestOfferIdx = priceWithOffers.indexOf(priceWithOffers.min())
+
+        if(bestOfferIdx == -1 || priceWithOffers[bestOfferIdx] >= books.map{book -> book.price}.sum()) {
+            return Pair(null, books.map{book -> book.price}.sum())
+        }
+
         return Pair(offers[bestOfferIdx], priceWithOffers[bestOfferIdx])
     }
 
     fun getPriceWithOffer(books: List<Book>, offer: CommercialOffer): Float {
-        return when(offer.type) {
+        if(offer.value < 0) return books.map{book -> book.price}.sum()
+        val result = when(offer.type) {
             OfferType.PERCENTAGE -> percentage(books, offer.value)
             OfferType.MINUS -> minus(books, offer.value)
             OfferType.SLICE -> slice(books, offer.value, offer.sliceValue)
         }
+        return if (result >= 0f) result else 0f
     }
 
     private fun percentage(books: List<Book>, value: Float): Float {
-        return books.map{book -> book.price}.sum() * (value / 100)
+        val priceSum = books.map{book -> book.price}.sum()
+        return priceSum - (priceSum * (value / 100))
     }
 
     private fun minus(books: List<Book>, value: Float): Float {
@@ -30,8 +38,8 @@ object OfferCalculator {
     }
 
     private fun slice(books: List<Book>, value: Float, sliceValue: Float?): Float {
-        if (sliceValue == null) throw Error("No slice value")
         val priceSum = books.map{book -> book.price}.sum()
+        if (sliceValue == null || sliceValue <= 0f) return priceSum
         return priceSum - (floor(priceSum / sliceValue) * value)
     }
 
